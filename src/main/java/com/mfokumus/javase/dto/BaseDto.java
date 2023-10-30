5617ee46d60e8386ca625aca57dd02514d611680
MeteFurkanOkumus <metefurkanokumus@gmail.com>
2023-10-30 21:40:44 +0300

    DTO

---
+++ src/main/java/com/mfokumus/javase/dto/BaseDto.java
@@ -0,0 +1,14 @@
+package com.mfokumus.javase.dto;
+
+
+import java.util.Date;
+
+public class BaseDto {
+
+    //Variable
+    private Long id;
+    private Date systemCreatedDate;
+
+
+    //Getter and Setter
+}
